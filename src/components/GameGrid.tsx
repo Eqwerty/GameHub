1a0b25cd8d8dd3b83951abfafbd2212315588ac5
Eduardo Quintana <edquintest@gmail.com>
2025-04-23 14:06:23 +0200

    Remove previous game cards when rendering skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGames from "../hooks/useGames";
+import useGames, { Game } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -11,11 +11,13 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
 
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{
           sm: 1,
@@ -26,20 +28,31 @@ const GameGrid = ({ gameQuery }: Props) => {
         spacing={3}
         padding="10px"
       >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+        {isLoading ? getGameCardSkeletons() : getGameCards(data)}
       </SimpleGrid>
     </>
   );
+};
+
+const getGameCards = (games: Game[]) => {
+  if (games.length === 0) {
+    return <Text>No games found</Text>;
+  }
+
+  return games.map((game) => (
+    <GameCardContainer key={game.id}>
+      <GameCard game={game} />
+    </GameCardContainer>
+  ));
+};
+
+const getGameCardSkeletons = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  return skeletons.map((skeleton) => (
+    <GameCardContainer key={skeleton}>
+      <GameCardSkeleton />
+    </GameCardContainer>
+  ));
 };
 
 export default GameGrid;
